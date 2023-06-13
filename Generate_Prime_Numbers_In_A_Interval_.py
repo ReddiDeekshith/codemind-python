75cb41a4aa79e24f2c6020bdd634b5ea9475f1b9
ReddiDeekshith <119839613+ReddiDeekshith@users.noreply.github.com>
2023-06-13 10:21:44 +0530

    def is_prime(n):     if(n==1):         return 0     for i in range(2,n):         if(n%i==0):             return 0     else:         return 1 a=int(input()) b=int(input()) for i in range(a,b+1):     if(is_prime(i)):         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,9 +1,13 @@
+def is_prime(n):
+    if(n==1):
+        return 0
+    for i in range(2,n):
+        if(n%i==0):
+            return 0
+    else:
+        return 1
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    for j in range(2,i):
-        if(i%j==0):
-            break;
-    else:
-        if(i>=2):
-            print(i)
+    if(is_prime(i)):
+        print(i)
