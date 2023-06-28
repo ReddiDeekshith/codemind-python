ab49928b46cdbee945887cd7f2a8e2b3b37de0bf
ReddiDeekshith <119839613+ReddiDeekshith@users.noreply.github.com>
2023-06-28 16:39:30 +0530

    def selfdiv(a):     s=str(a)     c=0     for i in s:         if(i=='0'):             return 0         elif(a%(int(i))==0):             c+=1     if(c==len(s)):         return 1     else:         return 0 a=int(input()) b=int(input()) for i in range(a,b+1):     if(selfdiv(i)):         print(i,end=' ')

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,14 +1,17 @@
+def selfdiv(a):
+    s=str(a)
+    c=0
+    for i in s:
+        if(i=='0'):
+            return 0
+        elif(a%(int(i))==0):
+            c+=1
+    if(c==len(s)):
+        return 1
+    else:
+        return 0
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    t=i
-    c=0
-    l=int(len(str(i)))
-    while(t!=0):
-        r=t%10
-        if(r!=0 and i%r==0):
-            c+=1
-        t=t//10
-    if(c==l):
-        print(i,end=" ")
-        
+    if(selfdiv(i)):
+        print(i,end=' ')
