79396cc477492d9983fdd8ba24d3ca9e4e12ae0c
ReddiDeekshith <119839613+ReddiDeekshith@users.noreply.github.com>
2023-06-28 16:32:33 +0530

    t=int(input()) import math for i in range(t):     a=int(input())     if(int((math.sqrt(a)))*int((math.sqrt(a)))==a):         print(True)     else:         print(False)

---
+++ Valid_Perfect_Square.py
@@ -0,0 +1,9 @@
+t=int(input())
+import math
+for i in range(t):
+    a=int(input())
+    if(int((math.sqrt(a)))*int((math.sqrt(a)))==a):
+        print(True)
+    else:
+        print(False)
+        
