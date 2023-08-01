05035b23429410c472ae34a12e6570b18b0f98f7
ReddiDeekshith <119839613+ReddiDeekshith@users.noreply.github.com>
2023-08-01 19:43:57 +0530

    m=int(input()) a=list(map(int,input().split())) c=0 for i in range(0,len(a)-2,2):     if (a[i]<a[i+1] and a[i+1]>a[i+2]) or (a[i]>a[i+1] and a[i+1]<a[i+2]):         c+=1     else:         print("no")         break else:     print("yes")

---
+++ Wave_array.py
@@ -0,0 +1,11 @@
+m=int(input())
+a=list(map(int,input().split()))
+c=0
+for i in range(0,len(a)-2,2):
+    if (a[i]<a[i+1] and a[i+1]>a[i+2]) or (a[i]>a[i+1] and a[i+1]<a[i+2]):
+        c+=1
+    else:
+        print("no")
+        break
+else:
+    print("yes")
